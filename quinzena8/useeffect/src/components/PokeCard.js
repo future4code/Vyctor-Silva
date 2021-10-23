0767ae52413ec10818988ac0a778cb82facbb446
Vyctorpierre <vyctorpierre2009@gmail.com>
2021-10-23 11:07:42 -0300

    versaofinal

--- quinzena8/useeffect/src/components/PokeCard.js
+++ quinzena8/useeffect/src/components/PokeCard.js
@@ -16,22 +16,23 @@ export const PokeCard = (props) => {
   // método que roda após a atualização do componente.
   // Um dos casos de atualização do componente é a
   // mudança da props que está sendo passado pra ele
-   useEffect((prevProps) => {
-    // aqui, é feita uma verificação da props anterior com a props atual.
-    // Caso a props anterior seja diferente da props atual,
-    // a função pegaPokemon é chamada.
-    if (prevProps.pokemon !== props.pokemon) {
-        pegaPokemon(props.pokemon);
-      }
-   }, [props.pokemon])
+  // aqui, é feita uma verificação da props anterior com a props atual.
+  // Caso a props anterior seja diferente da props atual,
+  // a função pegaPokemon é chamada.
+  useEffect (() => {
+    
+    pegaPokemon(props.pokemon)  //passando o state pokemon do App como props
+  }, [props.pokemon])     // e toda vez que o state pokemon for alterado, eu quero que seja reinderizado a tela
 
-  
+ 
+    
 
   // função que bate na poke API com um nome específico de pokemon
   // Isso permite que consigamos pegar as infos dos pokemons.
   // Nos métodos de ciclo de vida, ela é chamada passando como
   // parâmetro o nome de pokemon que está chegando como props.
   const pegaPokemon = (pokeName) => {
+    console.log(pokeName)
     axios
       .get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
       .then((response) => {
@@ -44,8 +45,6 @@ export const PokeCard = (props) => {
   };
 
   
-    
-
     return (
       <div>
         <p>{pokemon.name}</p>
